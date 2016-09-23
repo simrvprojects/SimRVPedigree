1a3aa662b8c786ad33fc74088a5eb3db89044986
cnieuwoudt <cnieuwou@sfu.ca>
2016-09-23 14:49:00 -0700

    FINALLY! Well, ... maybe, hopefully the vignette has been added the correct way.

---
+++ R/SimRVPedigree_doc.R
@@ -0,0 +1,10 @@
+#' Randomly Simulate Pedigrees Ascertained for Disease Status.
+#'
+#' The \code{SimRVPedigree} package allows random simulation of pedigrees likely to be asertained for multiple affected members under a particular study design.  Pedigrees include individual specific variables such as birth year, and, when applicable, onset year, and/or death year.  Additionally, we simualte the transmission of a rare variant, assumed to increase disease susceptibility, according to Mendel's laws.
+#'
+#' In addition to randomly simulating pedigrees, \code{SimRVPedigree} also includes functions for: simulating waiting times associated with a non-homogeneous Poisson process, randomly simulating life events contingent on user-provided age-specific hazards.
+#'
+#' @docType package
+#' @name SimRVPedigree
+NULL
+#> NULL
