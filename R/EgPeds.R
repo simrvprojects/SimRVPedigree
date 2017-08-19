8efca76989afb535b35eb4581f436898c863b7e5
cnieuwoudt <cnieuwou@sfu.ca>
2017-08-19 13:15:41 -0700

    Changed affected, available, and proband  to logical

--- R/EgPeds.R
+++ R/EgPeds.R
@@ -13,16 +13,16 @@
 #' \item{sex}{Gender identification variable: \code{sex = 0} for males, and \code{sex = 1} females. }
 #' \item{dadID}{Identification number of father}
 #' \item{momID}{Identification number of mother}
-#' \item{affected}{Affection status: \code{affected = 1} if individual has developed lymphoid cancer, and 0 otherwise.}
+#' \item{affected}{Affection status: \code{affected = TRUE} if individual has developed lymphoid cancer, and \code{FALSE} otherwise.}
 #' \item{DA1}{Paternally inherited allele at the assumed disease locus: \code{DA1 = 1} if rare variant is present, and 0 otherwise.}
 #' \item{DA2}{Maternally inherited allele at the assumed disease locus: \code{DA2 = 1} if rare variant is present, and 0 otherwise.}
 #' \item{birthYr}{Year of birth}
 #' \item{onsetYr}{Year of disease onset, when applicable.}
 #' \item{deathYr}{Year of death, when applicable.}
 #' \item{RR}{The subject's relative-risk of disease}
-#' \item{available}{Availability status, \code{available  = 0} if the individual is unavailable, and 1 otherwise}
+#' \item{available}{Availability status, \code{available  = TRUE} if the individual is unavailable, and \code{FALSE} 1 otherwise}
 #' \item{Gen}{The subject's generation number relative to the founder who introduced the rare variant.  That is, the founder who introduced the rare variant will have \code{Gen = 1}, his or her offspring will have \code{Gen = 2}, etc.}
-#' \item{proband}{Proband identification variable, \code{proband = 1} if the individual is the proband, and 0 otherwise.}
+#' \item{proband}{Proband identification variable, \code{proband = TRUE} if the individual is the proband, and \code{FALSE} otherwise.}
 #' }
 #'
 "EgPeds"
