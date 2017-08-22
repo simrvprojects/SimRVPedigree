2b35f5a7f6281414fd66106fbf4a6db759670435
cnieuwoudt <cnieuwou@sfu.ca>
2017-08-22 12:41:57 -0700

    fixed trim ped test

--- tests/testthat/test_trim.R
+++ tests/testthat/test_trim.R
@@ -1,7 +1,12 @@
 data("EgPeds")
 context("trim.ped")
 
-eggped <- ped(EgPeds)
+eggped <- new.ped(EgPeds)
 test_that("returns an error when no proband selected", {
-  expect_error(trim.ped(ped_file = eggped[eggped$FamID == 1, c(1,14)]))
+  expect_error(trim.ped(eggped[eggped$FamID == 1, c(1,14)]))
+})
+
+test_that("returns a pedigree no larger than the pedigree that was supplied", {
+  expect_lte(nrow(trim.ped(eggped[eggped$FamID == 1, ])),
+             nrow(eggped[eggped$FamID == 1, ]))
 })
