7ebcd41f34ac3814c3f240e896b5f14882b53699
cnieuwoudt <cnieuwou@sfu.ca>
2018-09-17 14:01:03 -0700

    fix example after adding restricted_BR argument

--- R/Utility_Functions.R
+++ R/Utility_Functions.R
@@ -111,25 +111,26 @@ find_available_parent <- function(ped, ID){
 #' data(AgeSpecific_Hazards)
 #'
 #'
-#' set.seed(2)
+#' set.seed(5)
 #' ex_ped <- sim_ped(hazard_rates = hazard(hazardDF = AgeSpecific_Hazards),
 #'                   GRR = 10, FamID = 1,
 #'                   founder_byears = c(1800, 1900),
+#'                   restricted_BR = TRUE,
 #'                   stop_year = 2020)
 #'
 #' plot(ex_ped)
 #'
-#' # Find most recent common ancestor of individuals with IDs 14 and 18
-#' find_mrca(ped = ex_ped, ID1 = 14, ID2 = 18)
+#' # Find most recent common ancestor of individuals with IDs 19 and 21
+#' find_mrca(ped = ex_ped, ID1 = 19, ID2 = 21)
 #'
 #' # Note that someone can be their own most recent common ancsetor.
-#' # In the following example, since the individual with ID 10 is the grandfather
-#' # of the individual with ID 22, the find_mrca function returns 10.
-#' find_mrca(ped = ex_ped, ID1 = 22, ID2 = 10)
+#' # In the following example, since the individual with ID 8 is the grandmother
+#' # of the individual with ID 21, the find_mrca function returns 8.
+#' find_mrca(ped = ex_ped, ID1 = 8, ID2 = 21)
 #'
 #' # For unrelated individuals, the find_mcra function returns NA
-#' find_mrca(ped = ex_ped, ID1 = 12, ID2 = 10)
-#' find_mrca(ped = ex_ped, ID1 = 21, ID2 = 19)
+#' find_mrca(ped = ex_ped, ID1 = 8, ID2 = 15)
+#' find_mrca(ped = ex_ped, ID1 = 5, ID2 = 4)
 #'
 find_mrca <- function(ped, ID1, ID2){
 
