11b2a900628209e97cf78553a7c1aa7a0d2f1b96
cnieuwoudt <cnieuwou@sfu.ca>
2019-03-19 10:43:47 -0700

    add sim_pop, this function is used for validation and may become internal

--- R/Validation_Functions.R
+++ R/Validation_Functions.R
@@ -1,4 +1,4 @@
-#' Simulate a sample of individuals
+#' Simulate survival data for a population sample
 #'
 #' @inheritParams sim_RVped
 #' @param nlives Numeric.  The number of individuals to simulate
@@ -15,7 +15,7 @@
 #' data(AgeSpecific_Hazards)
 #'
 #' a = Sys.time()
-#' s_dat = sim_pop(nlives = 1000,
+#' s_dat = sim_pop(nlives = 100,
 #'                 hazard_rates = hazard(AgeSpecific_Hazards[, c(1, 1, 2, 3)],
 #'                                       subtype_ID = c("HL", "NHL")),
 #'                 GRR = 10,
@@ -27,8 +27,9 @@
 #' head(s_dat)
 #' table(s_dat$RV_status)
 #' table(s_dat$affected)
-#' table(s_dat$onset_age)
+#' summary(s_dat$onset_age)
 #' table(s_dat$subtype)
+#' table(s_dat$RV_status, s_dat$subtype)
 #'
 #'
 sim_pop <- function(nlives, hazard_rates, GRR,
@@ -44,43 +45,79 @@ sim_pop <- function(nlives, hazard_rates, GRR,
   }
 
 
-  study_data <- data.frame(RV_status   = rvs,
-                           affected    = rep(NA, nlives),
-                           subtype     = rep(NA, nlives),
-                           onset_age   = rep(NA, nlives),
-                           death_age   = rep(NA, nlives),
-                           nchild      = rep(NA, nlives),
-                           first_birth = rep(NA, nlives))
-
-  for (k in 1:nlives) {
-    life_dat <- sim_life2(hazard_rates, GRR, carrier_prob,
-                          RV_status = study_data$RV_status[k], YOB, stop_year)
-
-    death_yr <- as.numeric(life_dat[[1]][1,])[which(colnames(life_dat[[1]])=="Death")]
-    onset_yr <- as.numeric(life_dat[[1]][1,])[which(colnames(life_dat[[1]])=="Onset")]
-
-    #tabulate life events so that we can count number of offspring
-    Life.counts <- table(colnames(life_dat[[1]]))
-
-    #count the number of children for each individual
-    study_data$nchild[k] <- ifelse(is.element("Child", names(Life.counts)),
-                                   Life.counts[which(names(Life.counts)=="Child")],
-                                   0)
-
-    study_data$first_birth[k] <- ifelse(is.element("Child", names(Life.counts)),
-                                        min(as.numeric(life_dat[[1]][1,])[which(colnames(life_dat[[1]])=="Child")]) - YOB,
-                                        NA)
-
-    #set Is.Onset = 1, if person develops cancer, otherwise set to zero.
-    study_data$affected[k] <- ifelse(is.element("Onset", names(Life.counts)), 1, 0)
-    study_data$subtype[k] <- life_dat[[2]]
-    study_data$onset_age[k] <- ifelse(is.element("Onset", names(Life.counts)),
-                                      onset_yr - YOB,
-                                      NA)
-    study_data$death_age[k] <- ifelse(is.element("Death", names(Life.counts)),
-                                      death_yr - YOB,
-                                      NA)
-  }
+  study_data <- do.call(rbind, lapply(1:nlives, function(x){
+    get_ind_data(hazard_rates, GRR,
+                 YOB, stop_year,
+                 RVstat = rvs[x],
+                 carrier_prob)
+  }))
+  # colnames(study_data) = c("RV_status", "affected",
+  #                          "subtype", "onset_age",
+  #                          "death_age", "censor_age",
+  #                          "nchild", "first_birth")
+  #
+  # study_data[, c(1, 2, 4:8)] <- as.numeric(study_data[, c(1, 2, 4:8)])
 
   return(study_data)
+}
+
+
+#' Get individual data for sim_pop function
+#'
+#' This is an internal function
+#'
+#' @inheritParams sim_pop
+#' @param RVstat The individual's cRV status
+#'
+#' @return A data frame of survival data
+#' @keywords internal
+get_ind_data <- function(hazard_rates, GRR,
+                         YOB, stop_year,
+                         RVstat,
+                         carrier_prob){
+
+  life_dat <- sim_life(hazard_rates, GRR, carrier_prob,
+                       RV_status = RVstat, YOB, stop_year)
+
+  # c(RV_status,
+  #   !is.na(life_dat$onset_event),
+  #   life_dat$subtype,
+  #   ifelse(is.na(life_dat$onset_event), NA,
+  #          life_dat$onset_event - as.numeric(life_dat$life_events[1])),
+  #   ifelse(is.na(life_dat$death_event), NA,
+  #          life_dat$death_event - as.numeric(life_dat$life_events[1])),
+  #   ifelse(is.na(life_dat$death_event),
+  #          life_dat$censor_year - as.numeric(life_dat$life_events[1]), NA),
+  #   length(life_dat$repro_events),
+  #   ifelse(length(life_dat$repro_events) > 0,
+  #          life_dat$repro_events[1] - as.numeric(life_dat$life_events[1]),
+  #          NA))
+
+#
+#   data.frame(RVstatus = RV_status,
+#              affected = !is.na(life_dat$onset_event),
+#              subtype = life_dat$subtype,
+#              onset_age = ifelse(is.na(life_dat$onset_event), NA, life_dat$onset_event - as.numeric(life_dat$life_events[1])),
+#              death_age = ifelse(is.na(life_dat$death_event), NA,
+#                                 life_dat$death_event - as.numeric(life_dat$life_events[1])),
+#              censor_age = ifelse(is.na(life_dat$death_event),
+#                                  life_dat$censor_year - as.numeric(life_dat$life_events[1]), NA),
+#              nchild = length(life_dat$repro_events),
+#              first_birth = ifelse(length(life_dat$repro_events) > 0,
+#                                   life_dat$repro_events[1] - as.numeric(life_dat$life_events[1]),
+#                                   NA))
+
+  return(data.frame(RV_status = RVstat,
+                    affected = !is.na(life_dat$onset_event),
+                    subtype = life_dat$subtype,
+                    onset_age = ifelse(is.na(life_dat$onset_event), NA, life_dat$onset_event - as.numeric(life_dat$life_events[1])),
+                    death_age = ifelse(is.na(life_dat$death_event), NA,
+                                       life_dat$death_event - as.numeric(life_dat$life_events[1])),
+                    censor_age = ifelse(is.na(life_dat$death_event),
+                                        life_dat$censor_year - as.numeric(life_dat$life_events[1]), NA),
+                    nchild = length(life_dat$repro_events),
+                    first_birth = ifelse(length(life_dat$repro_events) > 0,
+                                         life_dat$repro_events[1] - as.numeric(life_dat$life_events[1]),
+                                         NA)))
+
 }
